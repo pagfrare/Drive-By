3dca533cfb93944784103f01583b609ea15e6888
FELPS9696 <140739673+FELPS9696@users.noreply.github.com>
2024-12-10 18:20:52 -0300

    Add files via upload

---
+++ menu_principal.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_jogar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Carro.tscn")
+
+func _on_opções_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu_opcoes.tscn")
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
