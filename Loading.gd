b5e0724cf148cfb9a812c04f8cb65af0b4350b91
FELPS9696 <140739673+FELPS9696@users.noreply.github.com>
2025-02-26 21:23:33 -0300

    Add files via upload

---
+++ Loading.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var progresso = []
+var loadbarra = 0
+var carro
+var cena
+var sucesso = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	carro = "res://Carro.tscn"
+	ResourceLoader.load_threaded_request(carro)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (sucesso == 1):
+		tela(cena)
+	loadbarra = ResourceLoader.load_threaded_get_status(carro, progresso)
+	$ProgressBar.value = progresso[0] * 100
+	if ($ProgressBar.value == 100):
+		$ProgressBar.visible = false
+		$ProgressBar2.visible = true
+	if (loadbarra == ResourceLoader.THREAD_LOAD_LOADED):
+		cena = ResourceLoader.load_threaded_get(carro)
+		sucesso = 1
+		$Msg.visible = true
+		tela(cena)
+
+func tela (proxcena):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().change_scene_to_packed(proxcena)
--- carro(player).gd
+++ carro(player).gd
@@ -1,44 +1,48 @@
 extends CharacterBody2D
 
-var potenciamotor = 1200 #pra controlar a aceleracao
+var potenciamotor = 1000 #pra controlar a aceleracao
 # aqui é onde o filho chora e a mãe não vẽ.....
 var volantemax = 20 #controla o quanto o carro vai virar
 var atrito = -50 #controla o atrito que desacelera o carro
 var resistAr = -1 #Resistência do ar
 var freio = -200 #freio do carro
 var tracaoBase = 8 # tração base das rodas
-var velocidademaxre = 350
+var velocidademaxre = 300
 var distRodas = 65 #distância entre as rodas do carro pra tentar fazer ele bonitin igual o real
 var direcaoVolante  #pra onde o carro tá virando
 var aceleracao = Vector2.ZERO
 var Fatrito
 var teste
-var tanqueCombustivel = 100.0
+var tanqueCombustivel = 200.0
+var controle
+
+@onready var area_2d: Area2D = $"../Area2D" #gasolina Pick Up
 
 func _physics_process(delta: float) -> void:
 		aceleracao = Vector2.ZERO
-		get_input()  # Take input from player
+		get_input()
 		calculaVirada(delta)
 		aplicaAtrito(delta)
 		velocity += aceleracao * delta
+		gastaCombustivel(delta)
 		teste = velocity
 		move_and_slide()
-		gastaCombustivel(delta)
 
 
 func get_input():
-	# Get steering input and translate it to an angle
+	controle = 0
 	var virar = Input.get_axis("Esquerda", "Direita")
-	direcaoVolante = virar * deg_to_rad(volantemax)
+	if tanqueCombustivel > 0:
+		direcaoVolante = virar * deg_to_rad(volantemax)
 
-	# If accelerate is pressed, apply engine power to the car's forward direction
 	if Input.is_action_pressed("Acelerar"):
 		if tanqueCombustivel > 0:
+			controle = 1
 			aceleracao = potenciamotor * transform.x #pelo oq entendi esse tranform.x serve pra calcular o vetor movimentação modificado
 
-	# If brake is pressed, apply braking force
 	if Input.is_action_pressed("Frear"):
-		aceleracao = freio * transform.x
+		if tanqueCombustivel > 0:
+			aceleracao = freio * transform.x
 
 func aplicaAtrito(delta):
 	if aceleracao == Vector2.ZERO and velocity.length() < 10: #para o carro se tiver muito lerdo
@@ -55,24 +59,18 @@ func calculaVirada(delta):
 	rodaDianteira += velocity.rotated(direcaoVolante) * delta
 	# Calcula pra onde o carro vai (eu espero)
 	var novaDirecao = rodaTraseira.direction_to(rodaDianteira)
-
-	# Choose the traction model based on the current speed
 	var tracao = tracaoBase
 	if velocity.length() > 1100:
 		tracao = tracaoBase * 0.25
-		atrito = 0
+		atrito = -70
 	else:
 		tracao = tracaoBase
 		atrito = -50
 
-	# Dot product represents how aligned the new heading is with the current velocity direction
 	var d = novaDirecao.dot(velocity.normalized())
-
-	# If not braking (d > 0), adjust the car velocity smoothly towards the new heading
 	if d > 0:
 		velocity = lerp(velocity, novaDirecao * velocity.length(), tracao * delta)
 
-	# If braking (d < 0), reverse the direction and limit the speed
 	if d < 0:
 		velocity = - novaDirecao * min(velocity.length(), velocidademaxre)
 	# muda o carro pra apontar pra onde tá indo
@@ -81,12 +79,21 @@ func calculaVirada(delta):
 #Combustível aqui ó
 func gastaCombustivel(delta):
 	if velocity.length() > 400:
-		tanqueCombustivel -= delta * 10
+		if controle == 1:
+			tanqueCombustivel -= delta * 10
+		else:
+			tanqueCombustivel -= delta * 4
 	if tanqueCombustivel <= 0:
 		tanqueCombustivel = 0
 	atualizaCombustivel()
 
 func atualizaCombustivel():
-	$"../Interface de Usuario/Barra do Combustivel".value = tanqueCombustivel
+	$"../Interface de Usuario/Barra do Combustivel".value = tanqueCombustivel / 2
 	var stylebox = $"../Interface de Usuario/Barra do Combustivel".get("theme_override_styles/background")
 	stylebox.bg_color.h = lerp(0, 200, tanqueCombustivel / 100)
+	
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	tanqueCombustivel += 40
+	area_2d.queue_free()
